8c3ce6ed254392016d44ffbd5794897560fbc63d
Hans <alvarohansorellana@gmail.com>
2024-11-05 13:52:39 +0100

    pug

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ const morgan = require("./middlewares/morgan");
 
 app.use(morgan(':method :url :status :param[id] - :response-time ms :body')); // script que activa morgan y su respuestas por terminal
 app.use(express.json());   // ! AL INICIO DE LA HOJA, configuracion de express para que pueda recibir JSON
+app.use(express.static('public'))
+
+//Motor de plantillas  PUG
+app.set('view engine', 'pug');
+app.set('views','./views');
 
 // Rutas
 const booksRoutes = require("./routes/books.routes")
 const productsRoutes = require("./routes/products_old.routes")
 const entriesRoutes = require("./routes/entries.routes")    
+
+//WEB RUTAS
+const productsWebRoutes = require("./routes/products.web.routes")
 
 
 app.get('/', function (req, res) {
@@ -37,6 +45,24 @@ app.get('/books', function (req, res) {
 app.use('/api/books',booksRoutes);
 app.use('/api/products',productsRoutes);
 app.use('/api/entries',entriesRoutes);
+
+
+// ! PARA PUG AQUI ABAJO!
+
+//http://localhost:3000/first_template
+app.get('/first_template', function(req, res){
+    res.render('first_view');
+ });
+
+app.get('/dynamic_view', function(req, res){
+    res.render('dynamic', {
+       name: "TutorialsPoint", 
+       url:"http://www.tutorialspoint.com"
+    });
+ });
+
+app.use('./products2', productsWebRoutes);
+
 
   // ! Para ruta no existente ...
 app.use("*", manage404); // Usar este cuando haya un middleware activado
